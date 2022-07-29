7d4216eba111d50f3e6f505f530ead9f6cebbf69
2100049 <2100049@g.i-seifu.jp>
2022-07-29 13:09:53 +0900

    ルーティングの修正

--- client/src/components/pages/index.js
+++ client/src/components/pages/index.js
@@ -1,3 +1,3 @@
-export { default as LandingPage } from "./landingPage"
 export { default as SignIn } from "./signIn"
 export { default as SignUp } from "./signUp"
+export { default as LandingPage } from "./landingPage"
