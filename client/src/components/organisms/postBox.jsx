ed50d43c82fa24b706ae009302264089ff248c34
unknown <nishant.meher.9595@gmail.com>
2022-09-26 11:34:12 +0900

    色々

--- client/src/components/organisms/postBox.jsx
+++ client/src/components/organisms/postBox.jsx
@@ -1,158 +1,166 @@
 import {
-    Avatar,
-    Box,
-    Button,
-    Card,
-    IconButton,
-    Modal,
-    Stack,
-    TextField,
+  Avatar,
+  Box,
+  Button,
+  Card,
+  IconButton,
+  Modal,
+  Stack,
+  TextField,
 } from '@mui/material'
 import PhotoSizeSelectActualIcon from '@mui/icons-material/PhotoSizeSelectActual'
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
 
 const PostBox = (props) => {
-    return (
-        <Card sx={{ textAlign: 'center', p: 1, borderRadius: '15px', mt: 2, boxShadow: 3 }}>
+  return (
+    <Card
+      sx={{
+        textAlign: 'center',
+        p: 1,
+        borderRadius: '15px',
+        mt: 2,
+        boxShadow: 3,
+      }}
+    >
+      <Box
+        sx={{
+          fontFamily: 'Arial',
+          width: '100%',
+          height: '170px',
+          borderRadius: '10px',
+        }}
+      >
+        <Box sx={{ padding: '20px' }}>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              mb: '20px',
+            }}
+          >
+            <Avatar
+              variant="rounded"
+              sx={{
+                width: '50',
+                height: '50',
+                borderRadius: '50%',
+                objectFit: 'cover',
+                mr: '20px',
+              }}
+            />
+            <TextField
+              name="text"
+              id="outlined-multiline-flexible"
+              variant="standard"
+              placeholder="今どうしてる?"
+              fullWidth
+              multiline
+              rows={2}
+              autoFocus
+              onClick={props.togglePostModalOpen}
+            />
+          </Box>
+          <Modal
+            open={Boolean(props.openPostModal)}
+            onClose={props.togglePostModalClose}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
             <Box
-                sx={{
-                    fontFamily: 'Arial',
-                    width: '100%',
-                    height: '170px',
-                    borderRadius: '10px',
-                }}
+              component={'form'}
+              onSubmit={props.handleSubmit}
+              sx={{
+                backgroundColor: 'white',
+                width: '400px',
+                py: 2,
+                px: 5,
+                borderRadius: '10px',
+              }}
             >
-                <Box sx={{ padding: '20px' }}>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            mb: '20px',
-                        }}
-                    >
-                        <Avatar
-                            variant="rounded"
-                            sx={{
-                                width: '50',
-                                height: '50',
-                                borderRadius: '50%',
-                                objectFit: 'cover',
-                                mr: '20px',
-                            }}
-                        />
-                        <TextField
-                            name="text"
-                            id="outlined-multiline-flexible"
-                            variant="standard"
-                            placeholder="今どうしてる?"
-                            fullWidth
-                            multiline
-                            rows={2}
-                            autoFocus
-                            onClick={props.togglePostModalOpen}
-                        />
-                    </Box>
-                    <Modal
-                        open={Boolean(props.openPostModal)}
-                        onClose={props.togglePostModalClose}
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                        }}
-                    >
-                        <Box
-                            component={'form'}
-                            onSubmit={props.handleSubmit}
-                            sx={{
-                                backgroundColor: 'white',
-                                width: '400px',
-                                py: 2,
-                                px: 5,
-                                borderRadius: '10px',
-                            }}
-                        >
-                            <Box textAlign="right">
-                                <IconButton onClick={props.togglePostModalClose}>
-                                    <CloseRoundedIcon />
-                                </IconButton>
-                            </Box>
+              <Box textAlign="right">
+                <IconButton onClick={props.togglePostModalClose}>
+                  <CloseRoundedIcon />
+                </IconButton>
+              </Box>
 
-                            <TextField
-                                name="text"
-                                variant="standard"
-                                placeholder="今どうしてる?"
-                                fullWidth
-                                multiline
-                                rows={3}
-                                autoFocus
-                            />
+              <TextField
+                name="text"
+                variant="standard"
+                placeholder="今どうしてる?"
+                fullWidth
+                multiline
+                rows={3}
+                autoFocus
+              />
 
-                            <Button
-                                variant="contained"
-                                type="submit"
-                                fullWidth
-                                sx={{
-                                    mt: 2,
-                                }}
-                            >
-                                投稿
-                            </Button>
-                        </Box>
-                    </Modal>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'space-between',
-                        }}
-                    >
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'space-between',
-                            }}
-                        >
-                            <Box sx={{ display: 'flex', ml: '50px' }}>
-                                <Box
-                                    sx={{
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        mr: '15px',
-                                        cursor: 'pointer',
-                                    }}
-                                >
-                                    <Stack direction="row" spacing={2}>
-                                        <Button
-                                            color="success"
-                                            startIcon={<PhotoSizeSelectActualIcon />}
-                                        >
-                                            写真
-                                        </Button>
-
-                                        <Button
-                                            color="primary"
-                                            startIcon={<PhotoSizeSelectActualIcon />}
-                                        >
-                                            ビデオ
-                                        </Button>
-
-                                        <Button
-                                            color="error"
-                                            startIcon={<PhotoSizeSelectActualIcon />}
-                                        >
-                                            Tag
-                                        </Button>
-                                    </Stack>
-                                </Box>
-                            </Box>
-                        </Box>
-                    </Box>
-                </Box>
+              <Button
+                variant="contained"
+                type="submit"
+                fullWidth
+                sx={{
+                  mt: 2,
+                }}
+              >
+                投稿
+              </Button>
             </Box>
-        </Card>
-    )
+          </Modal>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}
+            >
+              <Box sx={{ display: 'flex', ml: '50px' }}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    mr: '15px',
+                    cursor: 'pointer',
+                  }}
+                >
+                  <Stack direction="row" spacing={2}>
+                    <Button
+                      color="success"
+                      startIcon={<PhotoSizeSelectActualIcon />}
+                    >
+                      写真
+                    </Button>
+
+                    <Button
+                      color="primary"
+                      startIcon={<PhotoSizeSelectActualIcon />}
+                    >
+                      ビデオ
+                    </Button>
+
+                    <Button
+                      color="error"
+                      startIcon={<PhotoSizeSelectActualIcon />}
+                    >
+                      Tag
+                    </Button>
+                  </Stack>
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Card>
+  )
 }
 
 export default PostBox
