93f48715b87591b748069bc76628f1cb5f3cd799
unknown <nishant.meher.9595@gmail.com>
2022-10-12 10:40:07 +0900

    CLassCard

---
+++ client/src/components/molecules/classRoomCard.jsx
@@ -0,0 +1,79 @@
+import { AssignmentIndOutlined, FolderOpenOutlined } from '@material-ui/icons'
+import { Avatar, Box, Card, Hidden, IconButton } from '@mui/material'
+import React from 'react'
+
+const classCard = () => {
+  return (
+    <>
+      <Card
+        sx={{
+          width: 300,
+          border: 1,
+          borderRadius: 5,
+          overflow: 'hidden',
+          cursor: 'pointer',
+        }}
+      >
+        <Box
+          sx={{
+            backgroundColor: 'gray',
+            height: 90,
+            position: 'relative',
+            color: 'white',
+            padding: 10,
+            borderBottom: 1,
+          }}
+        >
+
+          <Box
+            sx={{
+              fontWeight: 600,
+              fontSize: 30,
+            }}
+          >
+            Name
+          </Box>
+          <Box
+            sx={{
+              position: 'absolute',
+              bottom: 12,
+              fontSize: 15,
+            }}
+          >
+            CreatorName
+          </Box>
+          <Avatar
+            sx={{
+              width: 56,
+              height: 56,
+              position: 'absolute',
+              right: 5,
+            }}
+            src="https://lh3.googleusercontent.com/a-/ACNPEu8jOmU15WzaLKlxcLdDbcewV5cSnXDt3FM3vMUX=s96-c"
+          />
+        </Box>
+        <Box
+          sx={{
+            height: 190,
+            borderBottom: 1,
+          }}
+        ></Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row-reverse',
+          }}
+        >
+          <IconButton>
+            <FolderOpenOutlined />
+          </IconButton>
+          <IconButton>
+            <AssignmentIndOutlined />
+          </IconButton>
+        </Box>
+      </Card>
+    </>
+  )
+}
+
+export default classCard
--- client/src/components/templates/classRoomLayout.jsx
+++ client/src/components/templates/classRoomLayout.jsx
@@ -1,14 +1,24 @@
+import { Box, Container } from "@mui/material"
+import { ClassRoomCard } from "components/molecules"
 import ClassRoomPageHeader from "components/organisms/classRoomPageHeader"
 
 
 
 const ClassRoomLayout = (props) => {
   return (
-    <>
+    <Box>
         <ClassRoomPageHeader
             classRoomPageHeaderContents={props.classRoomPageHeaderContents}
         />
-    </>
+        <Container
+        maxWidth="xl"
+        sx={{
+          p: 2,
+
+        }}>
+        <ClassRoomCard/>
+        </Container>
+    </Box>
   )
 }
 
