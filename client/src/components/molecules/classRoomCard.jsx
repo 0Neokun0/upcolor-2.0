4810a6c5829f5fd0ec5994ab81d7cb0ac946cb37
Nishant Meher <90218986+0Neokun0@users.noreply.github.com>
2022-10-15 19:47:44 +0900

    card designing

--- client/src/components/molecules/classRoomCard.jsx
+++ client/src/components/molecules/classRoomCard.jsx
@@ -3,75 +3,80 @@ import { Avatar, Box, Card, Hidden, IconButton } from '@mui/material'
 import React from 'react'
 
 const classCard = () => {
-  return (
-    <>
-      <Card
-        sx={{
-          width: 300,
-          border: 1,
-          borderRadius: 5,
-          overflow: 'hidden',
-          cursor: 'pointer',
-        }}
-      >
-        <Box
-          sx={{
-            backgroundColor: 'gray',
-            height: 90,
-            position: 'relative',
-            color: 'white',
-            padding: 10,
-            borderBottom: 1,
-          }}
+    return (
+        <>
+        <Card
+            sx={{
+            width: 300,
+            border: 1,
+            borderRadius: 5,
+            overflow: 'hidden',
+            cursor: 'pointer',
+            }}
         >
+            <Box
+            sx={{
+                backgroundColor: 'gray',
+                height: 2,
+                position: 'relative',
+                color: 'white',
+                padding: 10,
+                borderBottom: 1,
+            }}
+            >
+                <Box
+                    sx={{
+                    fontWeight: 600,
+                    fontSize: 30,
+                    overflow: 'hidden',
+                    }}
+                >
+                    Name
+                </Box>
 
-          <Box
+                    <Avatar
+                        sx={{
+                        width: 56,
+                        height: 56,
+                        position: 'absolute',
+                        right: 5,
+                        }}
+                        src="https://lh3.googleusercontent.com/a-/ACNPEu8jOmU15WzaLKlxcLdDbcewV5cSnXDt3FM3vMUX=s96-c"
+                    />
+            </Box>
+            <Box
             sx={{
-              fontWeight: 600,
-              fontSize: 30,
+                height: 190,
+                borderBottom: 1,
             }}
-          >
-            Name
-          </Box>
-          <Box
-            sx={{
-              position: 'absolute',
-              bottom: 12,
-              fontSize: 15,
-            }}
-          >
-            CreatorName
-          </Box>
-          <Avatar
-            sx={{
-              width: 56,
-              height: 56,
-              position: 'absolute',
-              right: 5,
-            }}
-            src="https://lh3.googleusercontent.com/a-/ACNPEu8jOmU15WzaLKlxcLdDbcewV5cSnXDt3FM3vMUX=s96-c"
-          />
-        </Box>
-        <Box
-          sx={{
-            height: 190,
-            borderBottom: 1,
-          }}
-        ></Box>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'row-reverse',
-          }}
-        >
-          <IconButton>
-            <FolderOpenOutlined />
-          </IconButton>
-          <IconButton>
-            <AssignmentIndOutlined />
-          </IconButton>
-        </Box>
-      </Card>
+            >
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        bottom: 15,
+                        ml: 2,
+                        fontSize: 15,
+                    }}
+                >
+                    CreatorName
+                </Box>
+            </Box>
+
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'row-reverse',
+                }}
+            >
+                <IconButton>
+                    <FolderOpenOutlined />
+                </IconButton>
+
+                <IconButton>
+                    <AssignmentIndOutlined />
+                </IconButton>
+            </Box>
+        </Card>
     </>
   )
 }
