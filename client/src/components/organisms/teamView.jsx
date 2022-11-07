ab87d58688c79476cff4bf61bdba66bb8420bbdf
unknown <nishant.meher.9595@gmail.com>
2022-11-07 16:47:22 +0900

    TeamworkCardまで

--- client/src/components/organisms/teamView.jsx
+++ client/src/components/organisms/teamView.jsx
@@ -1,4 +1,8 @@
-import { Box, Card, CardActionArea, Typography } from "@mui/material"
+import {  Box, Card, CardActionArea, CardMedia, Chip, Typography } from "@mui/material"
+
+import BadgeIcon from "@mui/icons-material/Badge";
+import GroupWorkIcon from "@mui/icons-material/GroupWork";
+import DescriptionIcon from "@mui/icons-material/Description";
 
 const TeamView = (props) => {
     return (
@@ -14,26 +18,94 @@ const TeamView = (props) => {
                     return (
                         <Card
                             key={team["team_work_id"]}
+                            variant="outlined"
                             sx={{
-                                width: "200px",
+                                bgcolor: "white",
+                                width: 400,
+                                height: 300,
+                                p: 2,
+                                mb: 2,
+                                flexDirection: "row",
+                                borderRadius: "25px",
+                                transition: "transform 0.3s, border 0.3s",
+                                "&:hover": {
+
+                                    transform: "translateY(-2px)",
+                                },
+                                "& > *": { minWidth: "clamp(0px, (360px - 100%) * 999,100%)" },
+
                             }}
                         >
-                            <CardActionArea
+                            <CardActionArea sx={{
+                                p: 1,
+                                borderRadius: 5.
+                            }}
                                 onClick={() => window.location.href = "/teamwork/" + team["team_work_id"]}
                             >
-                                <Typography
-                                    variant="h6"
+                                <Box sx={{
+                                    p: 1,
+                                    display: "inline-flex",
+                                    justifyContent: "center",
+                                }}>
+                                    <Chip sx={{
+                                        mt: 1.5,
+                                        mr: 1,
+                                    }}
+                                        icon={<BadgeIcon />}
+                                        label="作品"
+                                    />
+                                    <Typography
+                                        variant="h3"
+                                    >
+                                        {team["team_work_name"]}
+                                    </Typography>
+
+
+                                </Box>
+                                <CardMedia
+                                    component="img"
+                                    height="100"
+                                    image="https://images.pexels.com/photos/256395/pexels-photo-256395.jpeg?cs=srgb&dl=pexels-pixabay-256395.jpg&fm=jpg"
+                                />
+
+
+                                <Box sx={{
+                                    p: 1,
+                                    display: "inline-flex",
+                                    justifyItems: "center",
+                                }}
                                 >
-                                    {team["team_work_name"]}
-                                </Typography>
+                                    <Chip sx={{
+                                        mt: 1,
+                                        mr: 1,
+                                    }}
+                                        icon={<GroupWorkIcon />}
+                                        label="チーム" />
+                                    <Typography
+                                        variant="h5"
+                                        gutterBottom
+                                    >
+                                        {team["team_name"]}
+                                    </Typography>
+                                </Box>
+                                <Box sx={{
+                                    p: 1,
+                                    display: "flex",
+                                    textOverflow: "ellipsis",
+                                }}
+                                >
+                                    <Chip sx={{
+                                        mr: 1,
+                                        mb: 2,
+                                    }}
+                                        icon={<DescriptionIcon />}
+                                        label="説明"
+                                    />
+                                    <Typography variant="subtitle2" gutterBottom>
+                                        {team["team_work_description"]}
+                                    </Typography>
+                                </Box>
 
-                                <Typography>
-                                    {team["team_name"]}
-                                </Typography>
-
-                                <Typography>
-                                    {team["team_work_description"]}
-                                </Typography>
                             </CardActionArea>
                         </Card>
                     )
